13d638da962f2000987fcd73adce16dedc4099dd
Mathiios <mathioslima1@gmail.com>
2026-06-15 17:05:56 -0300

    feat: esconder barra de navegacao do Android em modo imersivo

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
 import { AuthProvider } from './src/context/AuthContext';
 import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setVisibilityAsync('hidden');
+      NavigationBar.setBehaviorAsync('overlay-swipe');
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <StatusBar style="dark" />
